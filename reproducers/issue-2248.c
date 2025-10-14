41b9c7183eab9470e55bb949a1fc695261786bf1
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2025-10-14 09:21:17 +0000

    Simplify issue 2248 reproducer

--- reproducers/issue-2248.c
+++ reproducers/issue-2248.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
 
-unsigned long long callme(unsigned long long _1, unsigned long long _2, unsigned long long a, unsigned long long b, unsigned long long c);
+size_t callme(size_t _1, size_t _2, size_t a, size_t b, size_t c);
 
 int main() {
-    unsigned long long ret = callme(0, 0, 0, 1, 2);
-	printf("%lld\n", ret);
-    return 0;
+    int64_t ret = callme(0, 0, 0, 1, 2);
+
+	int var = 0;
+
+	if (ret < 0)
+		var = 0;
+	else
+		var = 5;
+
+	return var;
 }
 
