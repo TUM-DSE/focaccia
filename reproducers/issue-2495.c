c247770524e182ac3b2ac14a7f7ecfadb0a76bec
Christian Krinitsin <mail@krinitsin.com>
2025-11-25 10:58:10 +0100

    Update reproducers

--- reproducers/issue-2495.c
+++ reproducers/issue-2495.c
@@ -1,20 +1,8 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
+char i_R8[8] = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+char i_MM0[8] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+char o_R8[8];
 
-uint8_t i_R8[8] = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
-uint8_t i_MM0[8] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-uint8_t o_R8[8];
-
-void __attribute__ ((noinline)) show_state() {
-    printf("R8: ");
-    for (int i = 0; i < 8; i++) {
-        printf("%02x ", o_R8[i]);
-    }
-    printf("\n");
-}
-
-void __attribute__ ((noinline)) run() {
+int main(int argc, char **argv) {
     __asm__ (
         ".intel_syntax noprefix\n"
         "mov r8, qword ptr [rip + i_R8]\n"
@@ -23,10 +11,5 @@ void __attribute__ ((noinline)) run() {
         "mov qword ptr [rip + o_R8], r8\n"
         ".att_syntax\n"
     );
-}
-
-int main(int argc, char **argv) {
-    run();
-    show_state();
     return 0;
 }
