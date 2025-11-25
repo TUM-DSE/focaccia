ede3d0c1b980f8527a45ae5b513bbcc3099818e2
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2025-11-25 16:36:52 +0000

    Add support for emulating read()/write() and vector variants

--- src/focaccia/qemu/x86.py
+++ src/focaccia/qemu/x86.py
@@ -2,6 +2,10 @@ from focaccia.qemu.syscall import SyscallInfo
 
 # Incomplete, only the most common ones
 emulated_system_calls = {
+    0: SyscallInfo('read', patchup_address_registers=['rsi']),
+    1: SyscallInfo('write'),
+    19: SyscallInfo('readv', patchup_address_registers=['rsi']),
+    20: SyscallInfo('writev'),
     34:  SyscallInfo('pause', []),
     39:  SyscallInfo('getpid', []),
     102: SyscallInfo('getuid', []),
