00750969f99ad6b3106bb42d022b3c5d1062365e
ReimersS <sebastian.reimers@tum.de>
2025-11-25 16:14:16 +0000

    Add read syscall

--- src/focaccia/qemu/x86.py
+++ src/focaccia/qemu/x86.py
@@ -2,6 +2,7 @@ from focaccia.qemu.syscall import SyscallInfo
 
 # Incomplete, only the most common ones
 emulated_system_calls = {
+    0:   SyscallInfo('read', patchup_address_registers=['rsi']),
     34:  SyscallInfo('pause', []),
     39:  SyscallInfo('getpid', []),
     102: SyscallInfo('getuid', []),
